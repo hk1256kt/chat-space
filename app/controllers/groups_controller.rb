b1d8612e671d79ee46fe676b3817da66153b3506
hk1256kt <60028619+hk1256kt@users.noreply.github.com>
2020-02-20 21:49:21 +0900

    complete_massage_send

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,9 @@ class GroupsController < ApplicationController
   def index
   end
 
-  def new
+  #空のグループテーブルを呼出してインスタンスを生成し、
+  #ローグインしているuser情報を取得し、
+  def new 
     @group = Group.new
     @group.users << current_user
   end
@@ -17,19 +19,23 @@ class GroupsController < ApplicationController
     end
   end
 
+  #グループテーブルからgrouo idを１つ取り出す
   def edit
     @group = Group.find(params[:id])
   end
 
+  #グループテーブルからgroup idを１つ取り出す。
+  #ストロングパラメーターのデータを
   def update
     @group = Group.find(params[:id])
-    if group.update(group_paramas)
-      redirect_to root_path, notice: 'グループを更新しました'
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
   end
 
+  #グループのパラメーターからnameとuseridを取り出している。
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
