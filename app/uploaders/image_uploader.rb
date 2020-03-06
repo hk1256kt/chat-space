4e9c825331dffb58b3457bb177281946a001c3db
hk1256kt <60028619+hk1256kt@users.noreply.github.com>
2020-03-06 15:46:43 +0900

    fix6-AWS

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :fog
   #添付する画像のサイズを調整
   process resize_to_fit: [100, 100]
   # Override the directory where uploaded files will be stored.
